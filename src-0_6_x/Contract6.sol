b17e0d5a8b20800186331e7c31b2d0713389faa7
Henrique Barcelos <16565602+hbarcelos@users.noreply.github.com>
2022-07-27 15:41:14 -0300

    feat: add multi-version content

---
+++ src-0_6_x/Contract6.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.12;
+
+import "forge-std/console2.sol";
+
+contract Contract6 {
+  constructor() public {
+    console2.log("This is a Solidity 0.6.x contract!");
+  }
+}
+
--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+import "forge-std/console2.sol";
+
+contract Contract {
+  constructor() {
+    console2.log("This is a Solidity 0.8.x contract!");
+  }
+}
